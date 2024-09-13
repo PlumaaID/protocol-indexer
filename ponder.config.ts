b6d6e329e29fcd2c7d16a1d8bac96d728adcacdd
ernestognw <ernestognw@gmail.com>
2024-09-13 00:07:22 -0600

    Keep only alchemy RPCs

--- ponder.config.ts
+++ ponder.config.ts
@@ -11,9 +11,6 @@ export default createConfig({
         http(
           `https://arbitrum-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`
         ),
-        rateLimit(http("https://rpc.ankr.com/arbitrum"), {
-          requestsPerSecond: 5,
-        }),
       ]),
     },
     "arbitrum-sepolia": {
@@ -22,9 +19,6 @@ export default createConfig({
         http(
           `https://arbitrum-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`
         ),
-        rateLimit(http("https://sepolia-rollup.arbitrum.io/rpc"), {
-          requestsPerSecond: 5,
-        }),
       ]),
     },
     polygon: {
@@ -33,25 +27,23 @@ export default createConfig({
         http(
           `https://polygon-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`
         ),
-        rateLimit(http("https://rpc.ankr.com/polygon"), {
-          requestsPerSecond: 5,
-        }),
       ]),
     },
   },
   contracts: {
     Endorser: {
       abi: erc721ABI,
-      address: "0x0000c908D1104caD2867Ec2A8Bb178D78C9bAaaa",
       filter: {
         event: "Transfer",
       },
       network: {
         "arbitrum-sepolia": {
           startBlock: 72138379,
+          address: "0x0000c908D1104caD2867Ec2A8Bb178D78C9bAaaa",
         },
         arbitrum: {
           startBlock: 244339084,
+          address: "0x0000c908D1104caD2867Ec2A8Bb178D78C9bAaaa",
         },
       },
     },
